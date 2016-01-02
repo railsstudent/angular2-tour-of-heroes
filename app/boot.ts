c26ad795b946f2fac23761944dc337085add7c28
Connie Leung <cleung334@gmail.com>
2016-01-02 21:37:20 +0800

    D D D C C C initial commit

---
+++ app/boot.ts
@@ -0,0 +1,7 @@
+import {bootstrap} from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+
+//We need two things to launch the application:
+// Angular's browser bootstrap function
+// The application root component that we just wrote.
+bootstrap(AppComponent);
